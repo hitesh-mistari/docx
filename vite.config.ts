563153fd220afd6d9ba7dfe6a0a7e1adbfa5075d
Hitesh Mistari <71367296+hitesh-mistari@users.noreply.github.com>
2025-05-27 21:34:02 +0530

    Fix nodePolyfills import statement

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'rollup-plugin-node-polyfills';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [react()],
